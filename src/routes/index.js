4ed1c5c146eea69c77ad501d431d4ed02e86d401
Rifki Andriyanto <rifkiandriyanto@users.noreply.github.com>
2020-03-15 16:46:29 +0700

    fix index route

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,39 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-const express = require('express');
-const Route = express.Router();
-const posRouter = require('./product');
-const userRoute = require('./user');
-const transactionRouter = require('./transaction');
-// const categoryRouter = require('./category')
-=======
 const express = require('express')
 const route = express.Router()
 const posRouter = require('./product')
 const userRoute = require('./user')
 const transactionRouter = require('./transaction')
 const categoryRouter = require('./category')
->>>>>>> cb1b2368382dd660433fe450c36a8544ca15e094
 
-route
-
+Route
   .use('/product', posRouter)
   .use('/user', userRoute)
   .use('/uploads', express.static('./uploads'))
-<<<<<<< HEAD
-  .use('/transaction', transactionRouter);
-// .use('/category', categoryRouter)
-=======
-const express = require('express')
-const Route = express.Router()
-const transactionRouter = require('./transaction')
-
-Route
-  .use('/transaction', transactionRouter)
->>>>>>> transaction
-=======
   .use('/transaction', transactionRouter)
   .use('/category', categoryRouter)
->>>>>>> cb1b2368382dd660433fe450c36a8544ca15e094
 
 module.exports = route;
