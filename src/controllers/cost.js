395b0bd52d41c9bccb7027c2f5d36c58f12466da
Rifki Andriyanto <57374907+rifkiandriyanto@users.noreply.github.com>
2020-03-18 18:48:28 +0700

    Update cost.js

--- src/controllers/cost.js
+++ src/controllers/cost.js
@@ -1,13 +1,13 @@
 const axios = require('axios')
 
 exports.cost = (req, res) => {
-  const dataParse = ({
+  const data = ({
     origin: req.body.origin,
     destination: req.body.destination,
     weight: req.body.weight,
     courier: req.body.courier
   })
-  axios.post('https://api.rajaongkir.com/starter/cost', dataParse, {
+  axios.post('https://api.rajaongkir.com/starter/cost', data, {
     headers: {
       key: '01c67c78ff9e384fdbac1775307db363'
     }
